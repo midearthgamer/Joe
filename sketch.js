80d39bacf6c7e7d6c56fe34a4a41c621074a2a55
midearthgamer <93051912+midearthgamer@users.noreply.github.com>
2022-06-21 18:03:21 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,24 +11,30 @@ function setup(){
   hypnoticBall.shapeColor = "red";
 
 
+
   var hypnoticBallPosition = database.ref('ball/position');
   hypnoticBallPosition.on("value", readPosition, showError);
 }
 
 function draw(){
+
+  if(hypnoticBallPosition.x > 300 || hypnoticBallPosition.x < -300 || hypnoticBallPosition.y < -300 || hypnoticBallPosition.y > 300) {
+
+  }
+
   background("white");
   
     if(keyDown(LEFT_ARROW)){
-      writePosition(-1,0);
+      writePosition(-3,0);
     }
-    if(keyDown(RIGHT_ARROW)){
-      writePosition(1,0);
+    else if(keyDown(RIGHT_ARROW)){
+      writePosition(3,0);
     }
-    if(keyDown(UP_ARROW)){
-      writePosition(0,-1);
+    else if(keyDown(UP_ARROW)){
+      writePosition(0,-3);
     }
-    if(keyDown(DOWN_ARROW)){
-      writePosition(0,+1);
+    else if(keyDown(DOWN_ARROW)){
+      writePosition(0,+3);
     }
     drawSprites();
   
