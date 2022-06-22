f6afcd597870a3cbfbd3447083a8c50ec56a3c1e
midearthgamer <93051912+midearthgamer@users.noreply.github.com>
2022-06-21 20:21:17 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,17 @@
 var hypnoticBall, database;
 var position;
 
+var c1,c2,c3
 
 function setup(){
   database = firebase.database();
   console.log(database);
-  createCanvas(500,500);
+  createCanvas(600,600);
 
   hypnoticBall = createSprite(250,250,10,10);
-  hypnoticBall.shapeColor = "red";
+  hypnoticBall.shapeColor = "blue";
 
-
+  background(128,128,128);
 
   var hypnoticBallPosition = database.ref('ball/position');
   hypnoticBallPosition.on("value", readPosition, showError);
@@ -18,22 +19,31 @@ function setup(){
 
 function draw(){
 
-  background("white");
-  
+    background(c1,c2,c3)
+
     if(keyDown(LEFT_ARROW)){
-      writePosition(-3,0);
+      writePosition(-5,0);
     }
     if(keyDown(RIGHT_ARROW)){
-      writePosition(3,0);
+      writePosition(5,0);
     }
     if(keyDown(UP_ARROW)){
-      writePosition(0,-3);
+      writePosition(0,-5);
     }
     if(keyDown(DOWN_ARROW)){
-      writePosition(0,+3);
+      writePosition(0,+5);
     }
     drawSprites();
-  
+
+    if(hypnoticBall.y >= 600 || hypnoticBall.y <= 0 || hypnoticBall.x >= 600 || hypnoticBall.x <= 0) {
+      c1 = random(63,191)
+      c2 = random(63,191)
+      c3 = random(63,191)
+      database.ref('ball/position').set({
+        'x': 300,
+        'y': 300
+      })
+    }
 }
 
 function writePosition(x,y){
